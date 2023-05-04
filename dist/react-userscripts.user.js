6be8c9a0d282c5e7115c8ed86f97ffac2c45c2f4
lee <784852885@qq.com>
2023-05-04 16:50:44 +0800

    upd: 修改文案

--- dist/react-userscripts.user.js
+++ dist/react-userscripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     ytt-api-copy
 // @version  1.0.1
-// @description 将请求类型名称、返回值类型名称、函数名称、请求react hook 名称复制到剪贴板中。<br> Copy the names of request type, return value type, function name, and hook name to the clipboard.
+// @description 将请求类型名称、返回值类型名称、函数名称、请求react hook 名称复制到剪贴板中
 // @inject-into content
 
 // @include https://yapi.cqkqinfo.com/*
